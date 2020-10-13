2d0f4154194403175534dec1e9917d635e9e616e
dinghui <dinghui@bcbook.com>
2020-10-13 17:42:47 +0800

    add：选择登陆方式

---
+++ lib_home/src/main/java/com/whdx/home/ui/fragment/SelectLoginFragment.kt
@@ -0,0 +1,19 @@
+package com.whdx.home.ui.fragment
+
+import com.whdx.base.ui.fragment.BaseFragment
+import com.whdx.home.R
+
+/**
+ * @Description
+ * @Author dinghui
+ * @Date 2020/10/13 0013 17:16
+ */
+class SelectLoginFragment:BaseFragment() {
+    override fun setLayoutResId() = R.layout.fragment_select_login
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+}
